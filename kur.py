966062179b7dc0105ce11c3b83498bba412a8c1e
Manuel Guttmann <crack00r@gmail.com>
2023-11-16 13:47:31 +0100

    Button für optionalen Soundalarm + Bugfix für Pushover

--- kur.py
+++ kur.py
@@ -2,7 +2,7 @@ import sys
 import os
 import json
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, IntVar  # Importieren von IntVar für die Checkbox
 import threading
 import pygame
 from selenium import webdriver
@@ -17,6 +17,8 @@ import http.client
 file_path = "appointments.json"
 config = {}  # Globale Konfigurationsvariable
 
+
+            
 def get_resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -25,10 +27,17 @@ def get_resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 def play_sound():
-    pygame.mixer.init()
+    pygame.mixer.init()  # Initialisieren Sie den Mixer hier
     sound_path = get_resource_path("alert_sound.mp3")
     pygame.mixer.music.load(sound_path)
     pygame.mixer.music.play(loops=-1)
+
+def stop_sound(root):
+    if pygame.mixer.get_init() is not None:  # Überprüfen Sie, ob der Mixer initialisiert ist
+        pygame.mixer.music.stop()
+        pygame.mixer.quit()  # Beenden Sie den Mixer, nachdem der Sound gestoppt wurde
+    root.destroy()
+
 
 def send_pushover_notification(message, user_key, app_token):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
@@ -45,10 +54,6 @@ def show_popup(changes):
         tk.Label(root, text=change, padx=20, pady=10).pack()
     tk.Button(root, text="OK", command=lambda: stop_sound(root), padx=20, pady=20).pack()
     root.mainloop()
-
-def stop_sound(root):
-    pygame.mixer.music.stop()
-    root.destroy()
 
 def get_appointment_data(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -71,7 +76,7 @@ def get_appointment_data(html):
     print("Insgesamt gefundene Termine:", len(appointments))
     return appointments
 
-def save_config(kind_id, payer_id, count_children, age_min, age_max, pushover_user, pushover_token, window):
+def save_config(kind_id, payer_id, count_children, age_min, age_max, pushover_user, pushover_token, sound_alarm, window):
     config = {
         "cure_kind_id": kind_id,
         "cure_payer_id": payer_id,
@@ -79,7 +84,8 @@ def save_config(kind_id, payer_id, count_children, age_min, age_max, pushover_us
         "age_children_min": age_min,
         "age_children_max": age_max,
         "pushover_user": pushover_user,
-        "pushover_token": pushover_token
+        "pushover_token": pushover_token,
+        "sound_alarm": sound_alarm.get()  # Speichern des Zustands der Checkbox
     }
     with open('config.json', 'w') as config_file:
         json.dump(config, config_file)
@@ -143,14 +149,21 @@ def show_config_window():
     pushover_token = tk.Entry(window)
     pushover_token.grid(row=7, column=1)
 
+    # Sound Alarm Checkbox
+    sound_alarm = IntVar(value=config.get("sound_alarm", 1))  # Standardwert ist aktiviert
+    sound_alarm_checkbox = tk.Checkbutton(window, text="Soundalarm aktivieren", variable=sound_alarm)
+    sound_alarm_checkbox.grid(row=9, column=0, columnspan=2)
+
     # Speichern Button
     save_button = tk.Button(window, text="Speichern", command=lambda: save_config(
         cure_kind_options[cure_kind.get()], cure_payer_options[cure_payer.get()],
         count_children.get(), age_min.get(), age_max.get(),
-        pushover_user.get(), pushover_token.get(), window))
-    save_button.grid(row=8, column=1)
+        pushover_user.get(), pushover_token.get(), sound_alarm, window))
+    save_button.grid(row=10, column=1)
 
     window.mainloop()
+    
+    
     # Überprüfen Sie, ob Pushover-Einstellungen vorhanden sind
     if config.get("pushover_user") and config.get("pushover_token"):
         pushover_enabled = True
@@ -183,6 +196,15 @@ else:
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
+def send_notifications(changes, pushover_user, pushover_token, sound_alarm):
+    # Sound abspielen, wenn aktiviert
+    if sound_alarm:
+        play_sound()
+
+    # Senden einer Pushover-Benachrichtigung, wenn aktiviert
+    if pushover_enabled:
+        for change in changes:
+            send_pushover_notification(change, pushover_user, pushover_token)
 while True:
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://1crm.awosano.de/app/find_appointments/new")
@@ -229,14 +251,13 @@ while True:
         changes.append(f"Termin entfernt: {appointment}")
 
     if changes:
-        sound_thread = threading.Thread(target=play_sound)
-        sound_thread.start()
-        show_popup(changes)
+        # Starten eines neuen Threads für Benachrichtigungen
+        # Zugriff auf pushover_user und pushover_token aus dem config-Dictionary
+        notification_thread = threading.Thread(target=send_notifications, args=(changes, config.get("pushover_user"), config.get("pushover_token"), config.get("sound_alarm")))
+        notification_thread.start()
 
-        # Senden einer Pushover-Benachrichtigung, wenn aktiviert
-        if pushover_enabled:
-            for change in changes:
-                send_pushover_notification(change, pushover_user, pushover_token)
+        # Anzeigen des Popups
+        show_popup(changes)
 
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(list(new_appointment_set), file)
